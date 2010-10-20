5681931ce125191baf3523a2a63cdf2408c75394
Hampus Wessman <hampus.wessman@gmail.com>
2010-10-20 13:29:38 +0200

    Move start text to a HTML file.

--- StartPanel.cpp
+++ StartPanel.cpp
@@ -1,11 +1,19 @@
 #include "StartPanel.hpp"
-
+#include "util.hpp"
+#include <wx/html/htmlwin.h>
 
 StartPanel::StartPanel(wxWindow* parent)
     : wxPanel(parent)
 {
-    wxStaticText* label = new wxStaticText(this, wxID_ANY, wxT("Welcome to Metalink Editor 2\n\nThis is an empty metalink. Get started by either opening\nan existing metalink or adding a file to this one. You can\nadd a file from the metalink menu."), wxDefaultPosition, wxDefaultSize, wxST_NO_AUTORESIZE);
-    wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-    sizer->Add(label, 1, wxALL|wxEXPAND, 10);
-    SetSizerAndFit(sizer);
+    // HTML file
+    wxStandardPaths stdpaths = get_stdpaths();
+    wxFileName file(stdpaths.GetDataDir(), wxT("start.htm"));
+    // Create HTML widget
+    wxHtmlWindow* htmlwin = new wxHtmlWindow(this);
+    htmlwin->LoadFile(file);
+    // Layout
+    wxBoxSizer* sizer1 = new wxBoxSizer(wxVERTICAL);
+    sizer1->Add(htmlwin, 1, wxEXPAND, 0);
+    SetSizerAndFit(sizer1);
+    Layout();
 }
